64cbba44a60f7313a4cb5922ab5dc65e303dc0cf
ShobanaM8 <shoba.aradhya@gmail.com>
2023-04-23 14:05:56 +0530

    First Commit

---
+++ src/GitTesting/Test1Demo.java
@@ -0,0 +1,11 @@
+package GitTesting;
+
+public class Test1Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.err.println("Git testing successful");
+
+	}
+
+}
